beb5dea00b107ac77fd7f4df7cd0df97b0921e91
Suarez Jesus <suarezmolina17@gmail.com>
2018-07-19 00:22:28 -0300

    Master-page basic creation with a sidenav, card and toolbar component from angular material.

---
+++ src/app/app.material.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatSidenavModule
+} from '@angular/material';
+
+import {MatListModule} from '@angular/material/list';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatListModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatListModule
+  ]
+})
+export class MaterialModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './app.material.module';
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { BusinessCardComponent } from './business-card/business-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    BusinessCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
